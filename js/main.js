989e2e954759049faf0479100c2f91f8f29fd42e
jfehrenbacher <fehrenb1@seattleu.edu>
2016-09-06 16:27:14 -0700

    Getting rid of nav and dynInput on checkboxes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 
 // Text box pop up
-function dynInput(cbox) {
-	if (cbox.checked) {
-		var input = document.createElement("input");
-		input.type = "text";
-		input.id = cbox.value;
-		input.placeholder = input.id;
-		var div = document.createElement("div");
-		div.id = cbox.name;
-		div.appendChild(input);
-		document.getElementById("insertinputs").appendChild(div);
-	} else {
-		document.getElementById(cbox.name).remove();
-	}
-}
+// function dynInput(cbox) {
+// 	if (cbox.checked) {
+// 		var input = document.createElement("input");
+// 		input.type = "text";
+// 		input.id = cbox.value;
+// 		input.placeholder = input.id;
+// 		var div = document.createElement("div");
+// 		div.id = cbox.name;
+// 		div.appendChild(input);
+// 		document.getElementById("insertinputs").appendChild(div);
+// 	} else {
+// 		document.getElementById(cbox.name).remove();
+// 	}
+// }
 
 function capHillExplore() {
 	$('#venuename').html("Neumos");
